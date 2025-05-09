0df9c5c4c6b72c53cb9c2ece1d0fd6630b116077
Mario Holubar <mario.holubar@googlemail.com>
2025-05-09 22:53:35 +0200

    BlendPixel function

--- game.cpp
+++ game.cpp
@@ -69,6 +69,20 @@ void UndoMutation( int i )
 	lc[i] = c_;
 }
 
+inline void BlendPixel(Surface *screen, int X0, int Y0, double grayl, unsigned short Weighting, BYTE rl, BYTE gl, BYTE bl) {
+    COLORREF clrBackGround = screen->pixels[X0 + Y0 * SCRWIDTH];
+    BYTE rb = GetRValue(clrBackGround);
+    BYTE gb = GetGValue(clrBackGround);
+    BYTE bb = GetBValue(clrBackGround);
+    double grayb = rb * 0.299 + gb * 0.587 + bb * 0.114;
+    unsigned short w = grayl < grayb ? Weighting : (Weighting ^ 255);
+
+    BYTE rr = (rb > rl ? (((w * (rb - rl)) >> 8) + rl) : (((w * (rl - rb)) >> 8) + rb));
+    BYTE gr = (gb > gl ? (((w * (gb - gl)) >> 8) + gl) : (((w * (gl - gb)) >> 8) + gb));
+    BYTE br = (bb > bl ? (((w * (bb - bl)) >> 8) + bl) : (((w * (bl - bb)) >> 8) + bb));
+    screen->Plot(X0, Y0, RGB(rr, gr, br));
+}
+
 // -----------------------------------------------------------
 // DrawWuLine
 // Anti-aliased line rendering.
@@ -135,30 +149,9 @@ void DrawWuLine( Surface *screen, int X0, int Y0, int X1, int Y1, uint clrLine )
             intensity weighting for this pixel, and the complement of the
             weighting for the paired pixel */
             Weighting = ErrorAcc >> 8;
-
-            COLORREF clrBackGround = screen->pixels[X0 + Y0 * SCRWIDTH];
-            BYTE rb = GetRValue( clrBackGround );
-            BYTE gb = GetGValue( clrBackGround );
-            BYTE bb = GetBValue( clrBackGround );
-            double grayb = rb * 0.299 + gb * 0.587 + bb * 0.114;
-            unsigned short w = grayl < grayb ? Weighting : (Weighting ^ 255);
-
-            BYTE rr = (rb > rl ? (((w * (rb - rl)) >> 8) + rl) : (((w * (rl - rb)) >> 8) + rb));
-            BYTE gr = (gb > gl ? (((w * (gb - gl)) >> 8) + gl) : (((w * (gl - gb)) >> 8) + gb));
-            BYTE br = (bb > bl ? (((w * (bb - bl)) >> 8) + bl) : (((w * (bl - bb)) >> 8) + bb));
-            screen->Plot( X0, Y0, RGB( rr, gr, br ) );
-
-            clrBackGround = screen->pixels[X0 + XDir + Y0 * SCRWIDTH];
-            rb = GetRValue( clrBackGround );
-            gb = GetGValue( clrBackGround );
-            bb = GetBValue( clrBackGround );
-            grayb = rb * 0.299 + gb * 0.587 + bb * 0.114;
-            w = grayl < grayb ? Weighting : (Weighting ^ 255);
-
-            rr = (rb > rl ? (((w * (rb - rl)) >> 8) + rl) : (((w * (rl - rb)) >> 8) + rb));
-            gr = (gb > gl ? (((w * (gb - gl)) >> 8) + gl) : (((w * (gl - gb)) >> 8) + gb));
-            br = (bb > bl ? (((w * (bb - bl)) >> 8) + bl) : (((w * (bl - bb)) >> 8) + bb));
-            screen->Plot( X0 + XDir, Y0, RGB( rr, gr, br ) );
+			// Draw pixels
+            BlendPixel(screen, X0, Y0, grayl, Weighting, rl, gl, bl);
+            BlendPixel(screen, X0 + XDir, Y0, grayl, Weighting, rl, gl, bl);
         }
         /* Draw the final pixel, which is always exactly intersected by the line
         and so needs no weighting */
@@ -182,32 +175,9 @@ void DrawWuLine( Surface *screen, int X0, int Y0, int X1, int Y1, uint clrLine )
         intensity weighting for this pixel, and the complement of the
         weighting for the paired pixel */
         Weighting = ErrorAcc >> 8;
-
-        COLORREF clrBackGround = screen->pixels[X0 + Y0 * SCRWIDTH];
-        BYTE rb = GetRValue( clrBackGround );
-        BYTE gb = GetGValue( clrBackGround );
-        BYTE bb = GetBValue( clrBackGround );
-        double grayb = rb * 0.299 + gb * 0.587 + bb * 0.114;
-        unsigned short w = grayl < grayb ? Weighting : (Weighting ^ 255);
-
-        BYTE rr = (rb > rl ? (((w * (rb - rl)) >> 8) + rl) : (((w * (rl - rb)) >> 8) + rb));
-        BYTE gr = (gb > gl ? (((w * (gb - gl)) >> 8) + gl) : (((w * (gl - gb)) >> 8) + gb));
-        BYTE br = (bb > bl ? (((w * (bb - bl)) >> 8) + bl) : (((w * (bl - bb)) >> 8) + bb));
-
-        screen->Plot( X0, Y0, RGB( rr, gr, br ) );
-
-        clrBackGround = screen->pixels[X0 + (Y0 + 1 )* SCRWIDTH];
-        rb = GetRValue( clrBackGround );
-        gb = GetGValue( clrBackGround );
-        bb = GetBValue( clrBackGround );
-        grayb = rb * 0.299 + gb * 0.587 + bb * 0.114;
-        w = grayl < grayb ? Weighting : (Weighting ^ 255);
-
-        rr = (rb > rl ? (((w * (rb - rl)) >> 8) + rl) : (((w * (rl - rb)) >> 8) + rb));
-        gr = (gb > gl ? (((w * (gb - gl)) >> 8) + gl) : (((w * (gl - gb)) >> 8) + gb));
-        br = (bb > bl ? (((w * (bb - bl)) >> 8) + bl) : (((w * (bl - bb)) >> 8) + bb));
-
-        screen->Plot( X0, Y0 + 1, RGB( rr, gr, br ) );
+		// Draw pixels
+		BlendPixel(screen, X0, Y0, grayl, Weighting, rl, gl, bl);
+		BlendPixel(screen, X0, Y0 + 1, grayl, Weighting, rl, gl, bl);
     }
 
     /* Draw the final pixel, which is always exactly intersected by the line
